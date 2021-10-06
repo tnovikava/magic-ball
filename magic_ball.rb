e862c8974bef1f7a687db2fec01cf34bce60e2a9
Tatsiana Novikava <tatsiana.novikava.other@gmail.com>
2021-10-06 20:52:52 +0300

    add magic_ball to local master

---
+++ magic_ball.rb
@@ -0,0 +1,40 @@
+hello = [
+  "Who asks will get the answer:",
+  "Hello my dear friend. Answering your question...",
+  "Hello dude. Today your answer is:"
+]
+
+answers = [
+  # positive
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes — definitely",
+  "You may rely on it",
+
+  # partially positive
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Signs point to yes",
+  'Yes',
+
+  # netrual
+  "Reply hazy, try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+
+  # negative
+  "Don’t count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful"
+]
+
+puts hello.sample
+puts
+sleep(2)
+puts answers.sample
